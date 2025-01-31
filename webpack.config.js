88b041998d20476033d63126744010262a3618ac
Matthew Fors <mattfors@users.noreply.github.com>
2025-01-30 21:24:25 -0500

    fixing base href

--- webpack.config.js
+++ webpack.config.js
@@ -2,60 +2,71 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { DefinePlugin } = require('webpack');
 
-module.exports = {
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
     entry: {
-        main: './src/index.js',
-        presentation: './src/presentation/presentation.js'
+      main: './src/index.js',
+      presentation: './src/presentation/presentation.js'
     },
     module: {
-        rules: [
-            {
-                test: /\.md$/,
-                use: 'raw-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-                include: path.resolve(__dirname, 'src'),
-                exclude: path.resolve(__dirname, 'src/presentation/presentation.js')
-            }
-        ],
+      rules: [
+        {
+          test: /\.md$/,
+          use: 'raw-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+          include: path.resolve(__dirname, 'src'),
+          exclude: path.resolve(__dirname, 'src/presentation/presentation.js')
+        }
+      ],
     },
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            chunks: ['main'],
-            filename: 'index.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/about.html',
-            chunks: ['main'],
-            filename: 'about.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/presentation/presentation.html',
-            chunks: ['presentation'],
-            filename: 'presentation/index.html'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/presentation/qr-code.png', to: 'presentation/qr-code.png' }
-            ]
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'],
+        filename: 'index.html',
+        base: isProd ? '/ascs-capstone/' : '/',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/about.html',
+        chunks: ['main'],
+        filename: 'about.html',
+        base: isProd ? '/ascs-capstone/' : '/',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/presentation/presentation.html',
+        chunks: ['presentation'],
+        filename: 'presentation/index.html',
+        base: isProd ? '/ascs-capstone/' : '/',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'src/presentation/qr-code.png', to: 'presentation/qr-code.png' }
+        ]
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+      new DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(argv.mode),
+      })
     ],
     devServer: {
-        static: path.join(__dirname, 'dist'),
-        liveReload: true,
-        hot: true,
-        compress: true,
-        watchFiles: ['src/**/*'],
+      static: path.join(__dirname, 'dist'),
+      liveReload: true,
+      hot: true,
+      compress: true,
+      watchFiles: ['src/**/*'],
     },
+  };
 };
